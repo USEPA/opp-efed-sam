eba0f0dab409eca2e3515b48ffd7325ec4ffda90
triphook <hook.james@epa.gov>
2021-10-06 11:06:38 -0400

    added launcher script

--- dev/sandbox.py
+++ dev/sandbox.py
@@ -1,10 +1,7 @@
-import pandas as pd
-import numpy as np
+a = {"Dockerfile", "celery_cgi.py", "flask_pram.py", "pram_flask", "stack_setup.bat", "uwsgi.ini", "README.md", "dev-setup.txt", "flaskr_test.py", "pytest.ini", "stack_setup_env.ps1", "wsgi_flask.py", "README.txt", "flask_cgi.py", "hms_flask", "run_pytests.sh", "start_flask.sh", "UNLICENSE.txt", "flask_hms.py", "mongo_io.py", "run_pytests_docker.sh", "temp_config", "__init__.py", "flask_nta.py", "nta_flask", "setup.py", "travis_build.sh"}
 
-a = pd.read_csv("E:\opp-efed-data\sam\Results\summary.csv", index_col=0)
+b = {"Dockerfile", "app-data", "flask_cgi.py", "hms_flask", "run_pytests_docker.sh", "travis_build.sh", "README.md", "celery_cgi.py", "flask_hms.py", "mongo_io.py", "setup.py", "uwsgi.ini", "README.txt", "collected-static", "flask_nta.py", "nta_flask", "stack_setup.bat", "wsgi_flask.py", "UNLICENSE.txt", "collected_static", "flask_pram.py", "pram_flask", "stack_setup_env.ps1", "__init__.py", "core", "flaskr_test.py", "pytest.ini", "start_flask.sh", "__pycache__", "dev-setup.txt", "hms-data", "run_pytests.sh", "temp_config"}
 
-print(a.T)
+print(b - a)
 
-b = a.T.pivot(index=['cdl_1', 'cdl_13'], columns=['erosion', 'runoff'])
-
-print(b)
+print(a - b)
