8ead735437437262cff0401a60e9913fc1d8d4d8
Hook <Hook.James@epa.gov>
2022-06-29 14:37:06 -0400

    attempted fix to baseflow issue

--- dev/sandbox.py
+++ dev/sandbox.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-
-a = np.zeros((4, 4018, 78))
-b = pd.Series(np.zeros((78,))).values
-print(a.shape)
-print(b.shape)
-print(a[:2].shape)
-a[:2] *= b
+mean_runoff = 6
+q = np.array([3, 4, 5, 6, 7, 6, 5, 4, 3, 2])
+n_dates = q.size
+baseflow = q - np.subtract(q, mean_runoff, out=np.zeros(n_dates), where=(q > mean_runoff))
+print(baseflow)
