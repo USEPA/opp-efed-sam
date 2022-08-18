f458c6914a83522a58657ab893b47fed13108126
Hook <Hook.James@epa.gov>
2022-08-18 14:29:42 -0400

    * to &

--- dev/sandbox.py
+++ dev/sandbox.py
@@ -5,6 +5,6 @@ active_scenarios = [1, 3, 4]
 
 test_df = pd.DataFrame(np.array([[False, False], [True, True], [False, False], [False, False], [False, False], [False, False]]), columns=['a', 'b'])
 
-test_df.loc[np.array(active_scenarios), 'b'] *= True
+test_df.loc[np.array(active_scenarios), 'b'] &= True
 
 print(test_df)
